75b4554a886db44946f7c5553f609ee9ef2f54bd
sai kiran <saikiranvarma159@gmail.com>
2025-02-28 22:30:24 +0530

    python: encapsulation

--- python_basics/003-oops.py
+++ python_basics/003-oops.py
@@ -1,8 +1,11 @@
 class Shape:
+    base_type = "flat" # class attribute. this is global to class
     def __init__(self,type): # we will define the initial object parameters here. self is a must
         self.type = type
+        self._color = "red" # protected varible
+        self.__material = "plastic" # private variable
     
-    def length(self):
+    def length(self): # method
         print("you need to implement this function")
 
     def area(self):
@@ -20,7 +23,17 @@ class Rectangle(Shape):
         print("area: ",w*h)
 
 rect1 = Rectangle("rectangle")
+print("class attribute: ",rect1.base_type)
 print(rect1.type)
 rect1.length(5,3)
 rect1.area(5,3)
+
+Shape.base_type = "long flat base" # changed global class variable
+
+rect2 = Rectangle("rectangle")
+print("class attribute: ",rect2.base_type) # change is reflected in object
+print(rect2.type)
+rect2.length(3,3)
+rect2.area(3,3)
+print("color: ",rect2.__material)
 
